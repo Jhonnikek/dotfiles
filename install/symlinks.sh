ef51a5a8aff01eb0a378b7ed66b8469c2aaecc6d
Jhonnikek <jhonnierdiaz12@proton.me>
2026-01-10 11:41:18 -0500

    remove zen config

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -11,12 +11,6 @@ ln -sfn "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 mkdir -p "$HOME/.local/bin"
 ln -sfn "$HOME/dotfiles/dots/scripts/"* "$HOME/.local/bin"
 
-mkdir -p "$HOME/.zen/zen"
-ln -sfn "$HOME/dotfiles/dots/zen/profiles.ini" "$HOME/.zen/profiles.ini"
-ln -sfn "$HOME/dotfiles/dots/zen/prefs.js" "$HOME/.zen/zen/prefs.js"
-ln -sfn "$HOME/dotfiles/dots/zen/chrome" "$HOME/.zen/zen/chrome"
-cp "$HOME/dotfiles/dots/zen/sessionstore.jsonlz4" "$HOME/.zen/zen/sessionstore.jsonlz4"
-
 mkdir -p "$HOME/.config/Code - OSS/User/"
 ln -sfn "$HOME/dotfiles/dots/code/settings.json" "$HOME/.config/Code - OSS/User/settings.json"
 cat "$HOME/dotfiles/dots/code/extensions.txt" | xargs -L 1 code-oss --install-extension
