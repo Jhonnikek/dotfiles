ad036e2d9fad644d4fa3851e985738a13b610ebe
Jhonnikek <jhonnierdiaz12@proton.me>
2026-02-17 13:49:03 -0500

    update apps script

--- install/apps.sh
+++ install/apps.sh
@@ -1,52 +1,54 @@
 #!/usr/bin/env bash
 
 apps=(
-alacritty
-anytype-bin
-atuin
-bat
-btop
-cava
-code
-docker
-duf
-fastfetch
-fzf
-jq
-lazydocker
-lazygit
-less
-lsd
-midori-bin
-nvim
-openssh
-pacman-contrib
-postgresql
-python-uv
-starship
-ttf-cascadia-code-nerd
-zellij
-zoxide
-zsh
+  alacritty
+  anytype-bin
+  atuin
+  bat
+  btop
+  chafa
+  code
+  docker
+  duf
+  fastfetch
+  fzf
+  go
+  jq
+  lazydocker
+  lazygit
+  less
+  lsd
+  npm
+  nvim
+  openssh
+  pacman-contrib
+  postgresql
+  python-uv
+  starship
+  ttf-cascadia-code-nerd
+  wl-clipboard
+  zellij
+  zoxide
+  zsh
 )
 
 services=(
-    docker.service
-    postgresql.service
+  docker.service
+  postgresql.service
 )
 
-for app in "${apps[@]}";do
-    yay -S --needed --noconfirm "${app}"
+for app in "${apps[@]}"; do
+  yay -S --needed --noconfirm "${app}"
 done
 
 # enable services
 for service in "${services[@]}"; do
-    if ! systemctl is-enabled "$service" &>/dev/null; then
-        echo "Enabling $service"
-        sudo systemctl enable "$service"
-    else
-        echo -e "\n$service is already enabled"
-    fi
+  if ! systemctl is-enabled "$service" &>/dev/null; then
+    echo "Enabling $service"
+    sudo systemctl enable "$service"
+  else
+    echo -e "\n$service is already enabled"
+  fi
 done
 
 #load autin offline config
