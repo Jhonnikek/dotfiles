de1f11a9adb4f346cfe7e4645cbd49e2df677644
jhonnikek <jhonnierdiaz12@proton.me>
2026-02-20 15:06:12 -0500

    update nvim config

---
+++ dots/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        settings = {
+          python = {
+            pythonPath = "./.venv/bin/python",
+            analysis = {
+              reportUnreachable = "none",
+            },
+          },
+        },
+      },
+    },
+  },
+}
