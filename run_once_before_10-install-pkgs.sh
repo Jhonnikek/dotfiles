f65c55f112311e2a94008ebb269a2105e52b2571
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-02-21 12:57:43 -0500

    add package list and update scripts to work with chezmoi

---
+++ run_once_before_10-install-pkgs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+if ! command -v yay &>/dev/null; then
+  echo ":: Installig yay"
+  sudo pacman -S --needed --noconfirm git base-devel
+
+  echo ":: Cloning yay repo"
+  git clone https://aur.archlinux.org/yay.git /tmp/yay_install
+
+  echo ":: Compiling yay"
+  cd /tmp/yay_install
+  makepkg -si --noconfirm
+
+  cd $HOME
+  rm -rf /tmp/yay_install
+
+  echo ":: Yay installed"
+else
+  echo ":: Yay is already installed"
+fi
+
+echo -e "\n:: Installing packages\n"
+yay -S --needed --noconfirm - <packages.txt
+source "$HOME/dotfiles/install/services.sh"
+
+services=(
+  docker.service
+  postgresql.service
+)
+
+for service in "${services[@]}"; do
+  if ! systemctl is-enabled "$service" &>/dev/null; then
+    echo "Enabling $service"
+    sudo systemctl enable "$service"
+  else
+    echo -e "\n$service is already enabled"
+  fi
+done
---
+++ run_once_before_20-ssh.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo pacman -Syu --needed --noconfirm git openssh
+
+echo -e "\n:: Generating ssh key\n"
+mkdir -p ~/.ssh
+ssh-keyscan -t ed25519 github.com >>~/.ssh/known_hosts 2>/dev/null
+
+if [ ! -f ~/.ssh/id_ed25519 ]; then
+  ssh-keygen -t ed25519 -C "138178283+jhonnikek@users.noreply.github.com" -f ~/.ssh/id_ed25519 -q -N ""
+fi
+eval "$(ssh-agent -s)" >/dev/null
+ssh-add ~/.ssh/id_ed25519 2>/dev/null
+
+cat <<EOF
+
+:: ssh key:
+$(cat ~/.ssh/id_ed25519.pub)
+===========================
+
+EOF
