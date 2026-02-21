d92023abefacdefbeabf363b3548a8a7313838cb
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-02-21 14:57:17 -0500

    update icons script

--- run_once_before_30-icons.sh
+++ run_once_before_30-icons.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p "$HOME/.local/share/icons/"
-for i in "$HOME/dotfiles/icons/"*; do
+for i in "$HOME/.local/share/chezmoi/icons/"*; do
   tar -xzf "$i" -C "$HOME/.local/share/icons/"
 done
