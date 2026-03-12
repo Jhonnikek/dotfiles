8cc9f0b75cf92733bd003e835cef55bac0d5842e
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-03-12 14:20:37 -0500

    update keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -6,4 +6,7 @@ map("n", "<C-Right>", "<C-w>l", { remap = true })
 map("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 
-vim.keymap.set({ "n", "v" }, "d", [["_d]])
+map({ "n", "v" }, "d", [["_d]])
+
+map("v", '"', 'c"<C-r>""<Esc>')
+map("v", "'", "c'<C-r>\"'<Esc>")
