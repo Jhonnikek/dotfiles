68904c47c77ec17e32e3f3ae2e5e2eb940e181a2
Jhonnikek <138178283+Jhonnikek@users.noreply.github.com>
2026-04-03 10:57:11 -0500

    update scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,65 +1,52 @@
 #!/usr/bin/env bash
 
-if ! command -v yay &>/dev/null; then
-  echo ":: Installig yay"
+if command -v yay &>/dev/null; then
+  aur=yay
+  echo ":: Using $aur"
+elif command -v paru &>/dev/null; then
+  aur=paru
+  echo ":: Using $aur"
+else
+  aur=None
+  echo ":: No aur detected"
+fi
+
+if [ "$aur" = "None" ]; then
+  echo ":: Installing yay"
+
+  echo ":: Installing dependencies"
   sudo pacman -S --needed --noconfirm git base-devel
 
   echo ":: Cloning yay repo"
   git clone https://aur.archlinux.org/yay.git /tmp/yay_install
 
   echo ":: Compiling yay"
-  cd /tmp/yay_install
+  cd /tmp/yay_install || exit
   makepkg -si --noconfirm
 
-  cd $HOME
+  cd "$HOME" || exit
   rm -rf /tmp/yay_install
 
   echo ":: Yay installed"
-else
-  echo ":: Yay is already installed"
+  aur=yay
 fi
 
 apps=(
-  alacritty
-  atuin
-  bat
-  btop
-  docker
-  duf
-  fastfetch
-  fd
-  fzf
-  lazydocker
-  lazygit
-  less
-  lsd
-  markdownlint-cli
-  npm
-  nvim
-  obsidian
-  openssh
-  pacman-contrib
-  pgcli
-  postgresql
-  ripgrep
-  ruff
-  starship
-  tree
-  tree-sitter-cli
-  ttf-cascadia-code-nerd
-  ty
-  uv
-  wl-clipboard
-  yazi
-  zellij
-  zoxide
-  zsh
+  alacritty atuin bat btop docker duf fastfetch fd fzf lazydocker
+  lazygit less lsd npm nvim obsidian openssh pacman-contrib pgcli
+  postgresql ripgrep ruff starship tree tree-sitter-cli
+  ttf-cascadia-code-nerd ty uv wl-clipboard yazi zellij zoxide zsh
 )
 
 echo -e "\n:: Installing packages\n"
-for app in "${apps[@]}"; do
-  yay -S --needed --noconfirm "${app}"
-done
+"$aur" -S --needed --noconfirm "${apps[@]}"
+
+echo -e "\n:: Configuring Services\n"
+
+if [ ! -d "/var/lib/postgres/data" ] || [ -z "$(ls -A /var/lib/postgres/data 2>/dev/null)" ]; then
+  echo ":: Initializing db"
+  sudo -u postgres initdb -D /var/lib/postgres/data
+fi
 
 services=(
   docker.service
@@ -68,8 +55,8 @@ services=(
 
 for service in "${services[@]}"; do
   if ! systemctl is-enabled "$service" &>/dev/null; then
-    echo "Enabling $service"
-    sudo systemctl enable "$service"
+    echo "Enabling and starting $service"
+    sudo systemctl enable --now "$service"
   else
     echo -e "\n$service is already enabled"
   fi
